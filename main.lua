55816bbaa15749784855b44832ab5aaa2f9d0dda
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2019-02-12 12:44:46 -0500

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- Created by: Mr. Atkinson
+-- Created on: Feb 8, 2019
+--
+-- This file prints "Hello, World!" to the console
+-----------------------------------------------------------------------------------------
+
+display.setStatusBar(display.HiddenStatusBar)
+
+print( "Hello, World!")
